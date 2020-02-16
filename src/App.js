607c83768f0924ade057e27bb08208c983430932
moh7afiz90 <moh.7afiz@gmail.com>
2020-02-16 21:59:20 +0300

    refactored useEffect function

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import React, {useState, useEffect, Fragment} from 'react';
-import { Container } from '@material-ui/core';
+import React, { useState, useEffect, Fragment } from 'react'
+import { Container } from '@material-ui/core'
 import NavBar from './components/Navbar'
 import TopStoriesList from './components/TopStoriesList'
 import SearchArticleList from './components/SearchedArticleList'
 import nytimes from './api/nytimes'
 
+const API_KEY = `${process.env.REACT_APP_API_KEY}`
 
-const API_KEY =`${process.env.REACT_APP_API_KEY}`
-
-
-const App = () => {
+export default function App() {
   const [articles, setArticles] = useState([])
   const [topStories, setTopStories] = useState([])
 
-
-  const onSearchSubmit = async(searchTerm) => {
-    const response = await nytimes.get(`/search/v2/articlesearch.json?q=${searchTerm}&api-key=${API_KEY}`)
+  const onSearchSubmit = async searchTerm => {
+    const response = await nytimes.get(
+      `/search/v2/articlesearch.json?q=${searchTerm}&api-key=${API_KEY}`
+    )
     setArticles(response.data.response.docs)
   }
 
+  const getTopArticles = async () => {
+    const response = await nytimes.get(
+      `/topstories/v2/world.json?api-key=${API_KEY}`
+    )
+    setTopStories(response.data.results)
+  }
+  
   useEffect(() => {
-    const getTopArticles = async () => {
-      const response = await nytimes.get(`/topstories/v2/world.json?api-key=${API_KEY}`)
-      setTopStories(response.data.results)
-    }
     getTopArticles()
-  },[])
+  }, [])
 
-  
-  
   return (
     <Fragment>
       <NavBar onInvokingSearchTerm={onSearchSubmit}></NavBar>
       <Container>
-        {
-          articles.length === 0 || articles === undefined? 
-          <TopStoriesList topStories={topStories}></TopStoriesList>: 
-          articles.length === 0 || articles === undefined? <div>Error</div>: 
-          <SearchArticleList articles={articles}></SearchArticleList>}
+        {articles.length === 0 || articles === undefined ? (
+          <TopStoriesList topStories={topStories}></TopStoriesList>
+        ) : articles.length === 0 || articles === undefined ? (
+          <div>Error</div>
+        ) : (
+          <SearchArticleList articles={articles}></SearchArticleList>
+        )}
       </Container>
     </Fragment>
-  );
+  )
 }
-
-export default App;
--- src/components/SearchedArticleList.js
+++ src/components/SearchedArticleList.js
@@ -1,50 +1,45 @@
 import React from 'react'
-import { 
- Box,
- Grid,
- makeStyles
-} from "@material-ui/core";
+import { Box, Grid, makeStyles } from '@material-ui/core'
 import ArticleCard from './SearchedStoriesCard/ArticleCard'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   root: {
-    margin: "15px",
-    display: "flex",
-    justifyContent: "center"
+    margin: '15px',
+    display: 'flex',
+    justifyContent: 'center'
   }
-}));
+}))
 
-const SearchedArticleList = ({articles}) => {
-  const classes = useStyles();
+const SearchedArticleList = ({ articles }) => {
+  const classes = useStyles()
   const colWidth = {
     xs: 12,
     sm: 6,
     md: 4,
     lg: 4
-  };
+  }
 
-  
-
-  if(articles === undefined){
+  if (articles === undefined) {
     return <div>Loading</div>
-  } else if( articles.length > 0) {
-      return (<div className={classes.root}>
-        <Box width={{ xs: "100%", md: "80%" }}>
+  } else if (articles.length > 0) {
+    return (
+      <div className={classes.root}>
+        <Box width={{ xs: '100%', md: '80%' }}>
           <Grid container spacing={3}>
-              {articles.map((article, index) => {
-                return (
-                  <Grid item {...colWidth} key={index}>
-                    <ArticleCard article={article} />
-                  </Grid>
-                )
-              })}
+            {articles.map((article, index) => {
+              return (
+                <Grid item {...colWidth} key={index}>
+                  <ArticleCard article={article} />
+                </Grid>
+              )
+            })}
           </Grid>
         </Box>
-      </div>)
+      </div>
+    )
   } else {
-     return <div>No data avaliable</div>
+    return <div>No data avaliable</div>
   }
 }
-
 
 export default SearchedArticleList
